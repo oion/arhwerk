0f20c6dc062017a1971b767a888e6fff15e2c47c
Ovidiu Ion <ovidiu.ion@extendio.ro>
2025-03-19 17:26:20 +0200

    Favicon test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,16 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "ro",
       },
-      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+      link: [
+        {
+          rel: "icon",
+          type: "image/x-icon",
+          href:
+            (import.meta.env.NUXT_APP_BASE_URL
+              ? import.meta.env.NUXT_APP_BASE_URL
+              : "/") + "favicon.ico",
+        },
+      ],
     },
   },
 
